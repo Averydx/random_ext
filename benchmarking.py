7c206cc8f90104da71c57161ec1bc2444ea20602
Averydx <avery.drennan@gmail.com>
2023-09-13 21:15:03 -0700

    modified:   benchmarking.py 	new file:   requirements.txt

--- benchmarking.py
+++ benchmarking.py
@@ -1,16 +1,12 @@
 import Rand
-import config
-from scipy.stats import norm
 from time import perf_counter
-import matplotlib.pyplot as plt
 import numpy # Sometimes we want to use a numpy function even if CuPy is available
 
 try:
     import cupy as np
-    _USE_GPU = True
 except ModuleNotFoundError:
     import numpy as np
-    print("CuPy not available, only use_gpu=False will work")
+    print("CuPy not available, falling back onto numpy")
 
 gen = Rand.PCG(seed=20)
 gen2 = np.random.default_rng(5)
@@ -30,7 +26,7 @@ print(f"custom:{mean[0]}\nnumpy:{mean[1]}")
 n1= gen.standard_normal(100000)
 n2 = gen2.standard_normal(100000)
 print("custom: %d bytes" % (n1.size * n1.itemsize))
-print("numpy: %d bytes" % (n2.size * n2.itemsize))
+print("numpy/cupy: %d bytes" % (n2.size * n2.itemsize))
 
 
 
